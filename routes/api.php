998c8b77c95ba0d22891c9c81c87747c4e0391f2
safia-khoulaid <safiakhoulaid11@gmail.com>
2025-03-10 11:25:04 +0000

    ajouter les fonctionalité de controller

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+use App\Http\Controllers\CountryController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/user', [AuthController::class, 'user'])->middleware('auth:sanctum');
+
+
+Route::apiResource('countries', CountryController::class)->middleware('auth:sanctum');
+
+Route::post('/countries/{id}/flag', [CountryController::class, 'uploadFlag'])->middleware('auth:sanctum');
+Route::get('/countries/{id}/flag', [CountryController::class, 'getFlag']);
